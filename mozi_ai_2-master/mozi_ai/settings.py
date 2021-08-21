983e6fdaf43cac93617702a18ed55a6d8fa8b62f
ChangQingAAS <1736731090@qq.com>
2021-08-21 22:49:14 +0800

    0821

--- mozi_ai_2-master/mozi_ai/settings.py
+++ mozi_ai_2-master/mozi_ai/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = '0%b4-_%nhk8qnz&23unry5n&%u^7j7v85w_v1tf@7&0xc0+l#l'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -76,40 +74,43 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mozi_ai.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',     # 数据库引擎
-        'NAME': 'moziai',                             # 你要存储数据的库名，事先要创建之
-        'USER': 'admin',                          # 数据库用户名
-        'PASSWORD': 'HsfwAdmin@0415',             # 密码
-        'HOST': '123.57.137.210',                      # 主机
-        'PORT': '3306',                           # 数据库使用的端口
- }
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'NAME': 'moziai',  # 你要存储数据的库名，事先要创建之
+        'USER': 'dachuang',  # 数据库用户名
+        # 'PASSWORD': 'HsfwAdmin@0415',  # 密码
+        'PASSWORD': 'dachuang123',  # 密码
+        # 'HOST': '123.57.137.210',  # 主机
+        'HOST': '127.0.0.1',
+        'PORT': '3306',  # 数据库使用的端口
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -124,7 +125,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -133,7 +133,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-

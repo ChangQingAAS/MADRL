da88e560fbd583f72631a30c46c73aa2a8c40aa7
ChangQingAAS <1736731090@qq.com>
2021-09-05 10:18:53 +0800

    0905

--- examples_in_moziAI_platform_/demo2案例/DuelingDQN/buffer.py
+++ examples_in_moziAI_platform_/demo2案例/DuelingDQN/buffer.py
@@ -5,7 +5,8 @@ from collections import deque
 class ReplayBuffer:
     # 初始化
     def __init__(self, size):
-        # 初始化buffer唯一个双端队列（先进先出，即自带更新）
+        # 初始化buffer唯一个双端队列（先进先出，
+        # 即自带更新，无需自己写函数处理多出来的部分）
         self.buffer = deque(maxlen=size)
         self.mem_size = size
         self.len = 0
@@ -16,12 +17,12 @@ class ReplayBuffer:
         batch_size = min(batch_size, self.len)
         batch = random.sample(self.buffer, batch_size)
 
-        s_arr = np.float32([arr[0] for arr in batch])
-        a_arr = np.float32([arr[1] for arr in batch])
-        r_arr = np.float32([arr[2] for arr in batch])
-        s1_arr = np.float32([arr[3] for arr in batch])
+        state_arr = np.float32([arr[0] for arr in batch])
+        action_arr = np.float32([arr[1] for arr in batch])
+        reward_arr = np.float32([arr[2] for arr in batch])
+        state1_arr = np.float32([arr[3] for arr in batch])
 
-        return s_arr, a_arr, r_arr, s1_arr
+        return state_arr, action_arr, reward_arr, state1_arr
 
     # 返回buffer大小
     def len(self):

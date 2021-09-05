b26eca33bedd347aaca09b3961654ae6d1dd6e1a
ChangQingAAS <1736731090@qq.com>
2021-09-05 12:55:01 +0800

    0905

--- examples_in_moziAI_platform_/demo案例-DuelingDQN算法/DuelingDQN/buffer.py
+++ examples_in_moziAI_platform_/demo案例-DuelingDQN算法/DuelingDQN/buffer.py
@@ -3,20 +3,24 @@ import random
 from collections import deque
 
 class ReplayBuffer:
-    # 初始化
+
     def __init__(self, size):
-        # 初始化buffer唯一个双端队列（先进先出，
-        # 即自带更新，无需自己写函数处理多出来的部分）
+        # 初始化buffer唯一个双端队列（先进先出，即自带更新，无需自己写函数处理多出来的部分）
         self.buffer = deque(maxlen=size)
+        # buffer最大size
         self.mem_size = size
+        # buffer当前大小
         self.len = 0
 
     # 抽样
     def sample_memory(self, batch_size):
+
+        # 抽样
         batch = []
         batch_size = min(batch_size, self.len)
         batch = random.sample(self.buffer, batch_size)
-
+        
+        # 分开
         state_arr = np.float32([arr[0] for arr in batch])
         action_arr = np.float32([arr[1] for arr in batch])
         reward_arr = np.float32([arr[2] for arr in batch])
@@ -24,10 +28,11 @@ class ReplayBuffer:
 
         return state_arr, action_arr, reward_arr, state1_arr
 
-    # 返回buffer大小
+    # 返回buffer当前大小
     def len(self):
         return self.len
 
+    # 保存经验
     def store_transition(self, state, action, reward, state_):
         trainsition = (state, action, reward, state_)
         self.len += 1
--- examples_in_moziAI_platform_/demo案例-DuelingDQN算法/DuelingDQN/train.py
+++ examples_in_moziAI_platform_/demo案例-DuelingDQN算法/DuelingDQN/train.py
@@ -9,16 +9,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-from . import utils
-
-from .  import buffer
-from .  import model
+from . import noise
+from . import buffer
+from . import model
 
 BATCH_SIZE = 128  #
 LEARNING_RATE = 0.001  # 训练学习率
 GAMMA = 0.99  # 奖励随时间步的衰减因子
-TAU = 0.001  # model参数软更新系数
-
 
 class Trainer:
     '''训练器'''
@@ -27,19 +24,18 @@ class Trainer:
                  action_lim,
                  dev,
                  model_save_path,
-                 gamma=0.99,
+                 gamma=GAMMA,
                  epsilon=0.9,
-                 lr=0.01,
+                 lr=LEARNING_RATE,
                  n_actions=12,
                  input_dims=3,
                  mem_size=50000,
-                 batch_size=32,
+                 batch_size=BATCH_SIZE,
                  eps_min=0.01,
                  eps_dec=1e-5,
                  replace=10000,
                  algo='DuelingDQNAgent',
-                 chkpt_dir='tmp/dqn'
-                 ):
+                 chkpt_dir='tmp/dueling_dqn'):
         self.gamma = gamma
         self.epsilon = epsilon
         self.lr = lr
@@ -56,21 +52,21 @@ class Trainer:
 
         self.memory = buffer.ReplayBuffer(mem_size)
         self.q_eval = model.DuelingDeepQNetwork(self.lr,
-                                          self.n_actions,
-                                          input_dims=self.input_dims,
-                                          name= self.algo + '_q_eval',
-                                          chkpt_dir=self.chkpt_dir)
+                                                self.n_actions,
+                                                input_dims=self.input_dims,
+                                                name=self.algo + '_q_eval',
+                                                chkpt_dir=self.chkpt_dir)
 
         self.q_next = model.DuelingDeepQNetwork(self.lr,
-                                          self.n_actions,
-                                          input_dims=self.input_dims,
-                                          name= self.algo + '_q_next',
-                                          chkpt_dir=self.chkpt_dir)
+                                                self.n_actions,
+                                                input_dims=self.input_dims,
+                                                name=self.algo + '_q_next',
+                                                chkpt_dir=self.chkpt_dir)
 
         # self.action_dim = action_dim 即n_actions
-        self.action_lim = action_lim
+        self.action_lim = action_lim # 应该是最大值，TODO：验证一下
         self.iter = 0
-        self.noise = utils.OrnsteinUhlenbeckActionNoise(self.n_actions)
+        self.noise = noise.OrnsteinUhlenbeckActionNoise(self.n_actions)
         self.device = dev
         self.write_loss = write_loss
 
@@ -80,16 +76,15 @@ class Trainer:
 
     # 抽样
     def sample_memory(self):
-        state, action, reward, new_state= self.memory.sample_memory(
+        state, action, reward, new_state = self.memory.sample_memory(
             self.batch_size)
 
         states = T.tensor(state).to(self.q_eval.device)
         rewards = T.tensor(reward).to(self.q_eval.device)
-        # dones = T.tensor(done).to(self.q_eval.device)
         actions = T.tensor(action).to(self.q_eval.device)
         states_ = T.tensor(new_state).to(self.q_eval.device)
 
-        return states, actions, rewards, states_ 
+        return states, actions, rewards, states_
 
     # 选择动作，这里的选择动作放在了Class Agent里，考虑把它放入env里的可能
     def choose_action(self, observation, reward_now):
@@ -133,26 +128,25 @@ class Trainer:
         #                                                            actions]
 
         q_pred = T.add(V_s, (A_s - A_s.mean(dim=1, keepdim=True)))[0]
-        print("q_pred is ",q_pred)
+        print("q_pred is ", q_pred)
 
         q_next = T.add(V_s_,
                        (A_s_ - A_s_.mean(dim=1, keepdim=True))).max(dim=1)[0]
 
-        print("q_next is",q_next)
-        print("rewards is ",rewards)
-        rewards = torch.randn(1,3).type(torch.FloatTensor)
-        print("rewards is ",rewards)
-
+        print("q_next is", q_next)
+        print("rewards is ", rewards)
+        rewards = torch.randn(1, 3).type(torch.FloatTensor)
+        print("rewards is ", rewards)
 
         # q_next[dones] = 0.0
         q_target = rewards + self.gamma * q_next
 
-        print("q_target is ",q_target)
-        print("q_target.shape is ",q_target.shape)
+        print("q_target is ", q_target)
+        print("q_target.shape is ", q_target.shape)
 
         loss = self.q_eval.loss(q_target, q_pred).to(self.q_eval.device)
-        print("loss is ",loss)
-        print("loss.shape is ",loss.shape)
+        print("loss is ", loss)
+        print("loss.shape is ", loss.shape)
         # loss = loss.reshape(-1,1)
         # print("loss.shape is ",loss.shape)
 
@@ -174,7 +168,7 @@ class Trainer:
     #     if model_save_path == None:
     #         model_save_path = self.get_models_path()
     #     torch.save(self.target_actor.state_dict(),
-    #                model_save_path + str(episode_count) + '_actor.pt') 
+    #                model_save_path + str(episode_count) + '_actor.pt')
     #     print("%s：%s Models saved successfully" %
     #           (datetime.datetime.now(), episode_count))
     #     # print("%s：轮数:%s 决策步数:%s  Reward:%.2f" % (datetime.now(), _ep, step, reward_now))
@@ -195,7 +189,7 @@ class Trainer:
     #     utils.hard_update(self.target_critic, self.critic)
     #     print("Models loaded successfully")
 
-        # 保存模型
+    # 保存模型
     def save_models(self):
         self.q_eval.save_checkpoint()
         self.q_next.save_checkpoint()
@@ -205,14 +199,4 @@ class Trainer:
         self.q_eval.load_checkpoint()
         self.q_next.load_checkpoint()
 
-    #############  ADD  ###################
-    def choose_action(self, observation):
-        if np.random.random() > self.epsilon:
-            state = torch.tensor([observation],
-                             dtype=T.float).to(self.q_eval.device)
-            _, advantage = self.q_eval.forward(state)
-            action = torch.argmax(advantage).item()
-        else:
-            action = np.random.choice(self.action_space)
-
-        return action
+    
